87b21e709f6fd5b13ccb4a4ac2217f0b42cad891
R Sengupta <105560427+rxngxpta@users.noreply.github.com>
2025-05-29 01:25:56 +0530

    Update 4 - ForecastEngine.py

--- pages/4 - ForecastEngine.py
+++ pages/4 - ForecastEngine.py
@@ -76,7 +76,7 @@ open = df['Open']
 hi = df['High']
 lo = df['Low']
 close = df['Close']
-prof_df_close = pd.DataFrame({"ds": ind, "y": close})
+prof_df_close = pd.DataFrame({"ds": [ind], "y": [close]})
 prof_df_close
 prof_df_close = prof_df_close.reset_index()
 prof_df_close
