56fd8c4d1467687a319ddd7d5afa88ffebd24d24
R Sengupta <105560427+rxngxpta@users.noreply.github.com>
2025-05-29 15:46:57 +0530

    Update 4 - ForecastEngine.py

--- pages/4 - ForecastEngine.py
+++ pages/4 - ForecastEngine.py
@@ -67,31 +67,6 @@ st.header("This page is under the works and shall be open soon!")
 
 
 
-k1, k2, k3 = st.columns([4, 3, 4])
-with k1:
-    st.write(" ")
-with k2:
-    st.subheader("Forecast Plot")
-with k3:
-    st.write(" ")
-
-with st.container():
-    st.plotly_chart(a, use_container_width=True)
-with st.container():
-    st.plotly_chart(c, use_container_width=True)
-
-st.write("  ---------------------------------------------------------------  ")
-with st.container():
-    j1, j2, j3 = st.columns([3, 4, 3])
-    with j1:
-        st.write(" ")
-    with j2:
-        st.subheader(f"{stock} Price Trajectory")
-    with j3:
-        st.write(" ")
-
-st.plotly_chart(b, use_container_width=True)
-
 st.write("  ---------------------------------------------------------------  ")
 url_ytube = "https://www.youtube.com/@LedgrInc"
 url_fb = "https://www.facebook.com/share/1BnXaYvRzV/"
