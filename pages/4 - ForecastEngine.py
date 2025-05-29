afb6d31168a88402f57afab9d8902bf0ac82ca91
R Sengupta <105560427+rxngxpta@users.noreply.github.com>
2025-05-29 16:30:34 +0530

    Update 4 - ForecastEngine.py

--- pages/4 - ForecastEngine.py
+++ pages/4 - ForecastEngine.py
@@ -36,60 +36,132 @@ with st.sidebar:
 start_date = dt.datetime(2020, 1, 1)
 end_date = dt.datetime.today()
 pathtkr = f"{direc}/pages/appdata/tickerlist_y.csv"
-
+vidfile4 = open(f"{direc}/pages/appdata/imgs/Forecast-Anime.mp4", "rb")
+video_bytes4 = vidfile4.read()
 tickerdb = pd.read_csv(pathtkr)
-tickerlist = tickerdb["SYMBOL"]
+tickerlist = tickerdb["Symbol"]
 
 # Functions & Cached Resources ################################################
+
+
 @st.cache_data
 def getdata(stock):
+    stock = stock + ".NS"
     stock = yf.Ticker(stock)
-    df = stock.history(period='max')['Close']
+    df = stock.history(period='max')
     return df
 
+# Pagework 1 - Inputs #########################################################
 
+
+st.title(":Forecast Engine:")
 # Icons and Links ###########################
-
 ytube = f"{direc}/pages/appdata/imgs/ytube.svg"
 fbook = f"{direc}/pages/appdata/imgs/fbook.svg"
 insta = f"{direc}/pages/appdata/imgs/insta.svg"
 linkedin = f"{direc}/pages/appdata/imgs/linkedin.svg"
-ledgrblog = f"{direc}/pages/appdata/imgs/Ledgr_Logo_F2.png"
-fc1, fc2 = st.columns(2)
+ledgrblog = f"{direc}/pages/appdata/imgs/Ledgr_Logo_F1.png"
+fc1, fc2 = st.columns([2, 3])
 with fc1:
-    st.title(":Forecast Engine:")
-    st.write("Train Ledgr's AI Engines. Forecast Asset Prices.")
+    st.caption("Train Ledgr's AI Engines. Forecast Asset Prices.")
     st.info("Chart behaviour, predict price-ranges, observe trajectories.")
 with fc2:
-    st.video('https://youtu.be/tqOWAGEnKKQ?si=yW1nz3AVFKvUxGjA')
+    st.video(video_bytes4)
 st.write("    -----------------------------------------------------------    ")
-st.subheader("This page is under the works and shall be open soon!")
+stock = st.selectbox("Please Select a Security Symbol", tickerlist)
+
+df = getdata(stock)
+ind = df.index
+ind = ind.tz_convert(None)
+open = df['Open']
+hi = df['High']
+lo = df['Low']
+close = df['Close']
+prof_df_close = pd.DataFrame({"ds": ind, "y": df['Close']})
+# prof_df_close
+prof_df_close = prof_df_close.reset_index()
+
+# Pagework 2 - Forecasting  ###################################################
+
+m = Prophet()
+
+m.fit(prof_df_close)
+future_year = m.make_future_dataframe(periods=150)
+forecast_year = m.predict(future_year)
+m.plot(forecast_year)
+m.plot_components(forecast_year)
+a = plot_plotly(m, forecast_year)
+a.update_xaxes(title="Timeline", visible=True, showticklabels=True)
+a.update_yaxes(title="Predicted Prices (INR)", visible=True,
+               showticklabels=True)
+b = plot_components_plotly(m, forecast_year)
+b.update_xaxes(title="Timeline", visible=True, showticklabels=True)
+b.update_yaxes(title="Predicted Prices (INR)", visible=True,
+               showticklabels=True)
+dx = forecast_year.filter(["ds", 'yhat'], axis=1)
+dx = dx.set_index(['ds'])
+dx.rename(columns={'yhat': 'Predictions'}, inplace=True)
+c = px.line(dx)
+c.add_trace(go.Scatter(x=dx.index, y=df['Close'], name='Close'))
+c.update_xaxes(title='Timeline', showticklabels=True, visible=True)
+c.update_yaxes(title="Price Data", showticklabels=True, visible=True)
+c.update_layout(legend=dict(
+    orientation="h",
+    entrywidth=100,
+    yanchor="bottom",
+    y=1.02,
+    xanchor="right", x=1
+    ))
+k1, k2, k3 = st.columns([4, 3, 4])
+with k1:
+    st.write(" ")
+with k2:
+    st.subheader("Forecast Plot")
+with k3:
+    st.write(" ")
 st.write("  ---------------------------------------------------------------  ")
-url_ytube = "https://www.youtube.com/@LedgrInc"
-url_fb = "https://www.facebook.com/share/1BnXaYvRzV/"
-url_insta ='https://www.instagram.com/alphaledgr/'
-url_blog = 'https://www.alphaledgr.com/blog'
-url_linkedin = "https://www.linkedin.com/company/ledgrapp/"
+with st.container():
+    st.plotly_chart(a, use_container_width=True)
+    with st.expander("Get Forecast Data Here!"):
+        st.write(forecast_year.iloc[-150:])
+        st.write(forecast_year.iloc[-100:])
 st.write("  ---------------------------------------------------------------  ")
-column1, column2, column3, column4, column5 = st.columns([1, 1, 1, 2 , 1])
+with st.container():
+    st.plotly_chart(c, use_container_width=True)
+with st.container():
+    j1, j2, j3 = st.columns([3, 5, 3])
+    with j1:
+        st.write(" ")
+    with j2:
+        st.subheader(f"{stock} Price Trajectory")
+    with j3:
+        st.write(" ")
+
+st.plotly_chart(b, use_container_width=True)
+
+st.write("  ---------------------------------------------------------------  ")
+url = "https://www.alphaledgr.com/blog"
+
+column1, column2, column3, column4, column5 = st.columns([1,1,1,2,1])
 with column1:
-    st.image(ytube, '[Ledgr\'s YouTube Channel](%s)' % url_ytube)
+    st.image(ytube, 'Ledgr\'s YouTube Channel')
 with column2:
-    st.image(fbook, '[Ledgr\'s FaceBook Page ](%s)' % url_fb)
+    st.image(fbook, 'Ledgr\'s FaceBook Page')
 with column3:
-    st.image(linkedin,  '[Our LinkedIn Page ](%s)' % url_linkedin)
+    st.image(linkedin,  'Ledgr\'s LinkedIn Page')
 with column4:
     st.write(" ")
-    st.image(ledgrblog,  '[Ledgr\'s Blog ](%s)' % url_blog)
-    st.write(" ")
+    st.image(ledgrblog,  'Ledgr\'s own Blog', use_container_width=True)
+st.write("check out this [link](%s)" % url)
+st.markdown("check out this [link](%s)" % url)
 with column5:
-    st.image(insta,  '[Ledgr\'s @ Instagram ](%s)' % url_insta)
-# # ###################################################################
-with st.container():
-    f9, f10, f11 = st.columns([1, 5, 1])
-    with f9:
-        st.write(" ")
-    with f10:
-        st.caption(": | 2025 - 2026 | All Rights Resrved  ©  Ledgr Inc. | www.alphaLedgr.com | alphaLedgr Technologies Ltd. :")
-    with f11:
-        st.write(" ")
+    st.image(insta, 'Check out Ledgr\'s Instagram')
+#    st.image(insta, 'Ledgr @ Instagram')
+ft1, ft2, ft3 = st.columns([1, 5, 1])
+with ft1:
+    st.write(" ")
+with ft2:
+    st.caption(": | 2024 - 2025 | All Rights Reserved  ©  Ledgr Inc. | www.alphaLedgr.com | alphaLedgr Technologies Ltd. :")
+with ft3:
+    st.write(" ")
+
