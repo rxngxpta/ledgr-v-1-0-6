10afa5fd53d4c1536beb6fc251a9d7023a9d7e66
R Sengupta <105560427+rxngxpta@users.noreply.github.com>
2025-06-01 03:49:01 +0530

    Update 4 - ForecastEngine.py

--- pages/4 - ForecastEngine.py
+++ pages/4 - ForecastEngine.py
@@ -1,4 +1,4 @@
-# __author__ = 'R. Sengupta | r_xn'
+ # __author__ = 'R. Sengupta | r_xn'
 # __copyright__ = 'Copyright 2023, Ledgr | www.alphaLedgr.com'
 # __credits__ = ['r_xn, s.sengupta, adasgupta@gmail.com']
 # __license__ = 'Ledgr | alphaledgr.com'
@@ -72,7 +72,7 @@ open = df['Open']
 hi = df['High']
 lo = df['Low']
 close = df['Close']
-prof_df_close = pd.DataFrame({"ds": ind, "y": close})
+prof_df_close = pd.DataFrame({"ds": ind, "y": close}, index = "ds")
 prof_df_close = prof_df_close.reset_index()
 
 st.write(prof_df_close)
